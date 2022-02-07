277f208d71590093145821d330be20d540071fe0
Tien-Fong-Le-00 <tienfongle00@gmail.com>
2022-02-07 03:24:51 +0200

    Add project files.

---
+++ TelerikTask/RadForm1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace TelerikWinFormsApp9
+{
+    partial class RadForm1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.A1 = new Telerik.WinControls.UI.RadButton();
+            this.A2 = new Telerik.WinControls.UI.RadButton();
+            this.A3 = new Telerik.WinControls.UI.RadButton();
+            this.B1 = new Telerik.WinControls.UI.RadButton();
+            this.B2 = new Telerik.WinControls.UI.RadButton();
+            this.B3 = new Telerik.WinControls.UI.RadButton();
+            this.C1 = new Telerik.WinControls.UI.RadButton();
+            this.C2 = new Telerik.WinControls.UI.RadButton();
+            this.C3 = new Telerik.WinControls.UI.RadButton();
+            this.btnNewGame = new Telerik.WinControls.UI.RadButton();
+            this.btnExit = new Telerik.WinControls.UI.RadButton();
+            this.radPanel1 = new Telerik.WinControls.UI.RadPanel();
+            this.radWaitingBar1 = new Telerik.WinControls.UI.RadWaitingBar();
+            this.radWaitingBar2 = new Telerik.WinControls.UI.RadWaitingBar();
+            this.dotsLineWaitingBarIndicatorElement2 = new Telerik.WinControls.UI.DotsLineWaitingBarIndicatorElement();
+            this.dotsLineWaitingBarIndicatorElement1 = new Telerik.WinControls.UI.DotsLineWaitingBarIndicatorElement();
+            ((System.ComponentModel.ISupportInitialize)(this.A1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.A2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.A3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.B1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.B2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.B3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.C1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.C2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.C3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnNewGame)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnExit)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radPanel1)).BeginInit();
+            this.radPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.radWaitingBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radWaitingBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // A1
+            // 
+            this.A1.Location = new System.Drawing.Point(8, 31);
+            this.A1.Name = "A1";
+            this.A1.Size = new System.Drawing.Size(100, 100);
+            this.A1.TabIndex = 0;
+            this.A1.Click += new System.EventHandler(this.button_click);
+            // 
+            // A2
+            // 
+            this.A2.Location = new System.Drawing.Point(114, 31);
+            this.A2.Name = "A2";
+            this.A2.Size = new System.Drawing.Size(100, 100);
+            this.A2.TabIndex = 1;
+            this.A2.Click += new System.EventHandler(this.button_click);
+            // 
+            // A3
+            // 
+            this.A3.Location = new System.Drawing.Point(220, 31);
+            this.A3.Name = "A3";
+            this.A3.Size = new System.Drawing.Size(100, 100);
+            this.A3.TabIndex = 1;
+            this.A3.Click += new System.EventHandler(this.button_click);
+            // 
+            // B1
+            // 
+            this.B1.Location = new System.Drawing.Point(8, 137);
+            this.B1.Name = "B1";
+            this.B1.Size = new System.Drawing.Size(100, 100);
+            this.B1.TabIndex = 1;
+            this.B1.Click += new System.EventHandler(this.button_click);
+            // 
+            // B2
+            // 
+            this.B2.Location = new System.Drawing.Point(114, 137);
+            this.B2.Name = "B2";
+            this.B2.Size = new System.Drawing.Size(100, 100);
+            this.B2.TabIndex = 1;
+            this.B2.Click += new System.EventHandler(this.button_click);
+            // 
+            // B3
+            // 
+            this.B3.Location = new System.Drawing.Point(220, 137);
+            this.B3.Name = "B3";
+            this.B3.Size = new System.Drawing.Size(100, 100);
+            this.B3.TabIndex = 1;
+            this.B3.Click += new System.EventHandler(this.button_click);
+            // 
+            // C1
+            // 
+            this.C1.Location = new System.Drawing.Point(8, 243);
+            this.C1.Name = "C1";
+            this.C1.Size = new System.Drawing.Size(100, 100);
+            this.C1.TabIndex = 1;
+            this.C1.Click += new System.EventHandler(this.button_click);
+            // 
+            // C2
+            // 
+            this.C2.Location = new System.Drawing.Point(114, 243);
+            this.C2.Name = "C2";
+            this.C2.Size = new System.Drawing.Size(100, 100);
+            this.C2.TabIndex = 1;
+            this.C2.Click += new System.EventHandler(this.button_click);
+            // 
+            // C3
+            // 
+            this.C3.Location = new System.Drawing.Point(220, 243);
+            this.C3.Name = "C3";
+            this.C3.Size = new System.Drawing.Size(100, 100);
+            this.C3.TabIndex = 1;
+            this.C3.Click += new System.EventHandler(this.button_click);
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.BackColor = System.Drawing.Color.Aqua;
+            this.btnNewGame.Location = new System.Drawing.Point(22, 61);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(150, 100);
+            this.btnNewGame.TabIndex = 2;
+            this.btnNewGame.Text = "New Game";
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click_1);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.IndianRed;
+            this.btnExit.Location = new System.Drawing.Point(22, 167);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(150, 100);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Exit";
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // radPanel1
+            // 
+            this.radPanel1.BackColor = System.Drawing.Color.DimGray;
+            this.radPanel1.Controls.Add(this.radWaitingBar2);
+            this.radPanel1.Controls.Add(this.radWaitingBar1);
+            this.radPanel1.Controls.Add(this.btnNewGame);
+            this.radPanel1.Controls.Add(this.btnExit);
+            this.radPanel1.Location = new System.Drawing.Point(354, 22);
+            this.radPanel1.Name = "radPanel1";
+            this.radPanel1.Size = new System.Drawing.Size(200, 330);
+            this.radPanel1.TabIndex = 4;
+            // 
+            // radWaitingBar1
+            // 
+            this.radWaitingBar1.Location = new System.Drawing.Point(22, 273);
+            this.radWaitingBar1.Name = "radWaitingBar1";
+            this.radWaitingBar1.Size = new System.Drawing.Size(130, 24);
+            this.radWaitingBar1.TabIndex = 5;
+            this.radWaitingBar1.Text = "radWaitingBar1";
+            this.radWaitingBar1.WaitingIndicators.Add(this.dotsLineWaitingBarIndicatorElement1);
+            this.radWaitingBar1.WaitingIndicatorSize = new System.Drawing.Size(100, 14);
+            this.radWaitingBar1.WaitingSpeed = 80;
+            this.radWaitingBar1.WaitingStyle = Telerik.WinControls.Enumerations.WaitingBarStyles.DotsLine;
+            // 
+            // radWaitingBar2
+            // 
+            this.radWaitingBar2.Location = new System.Drawing.Point(22, 31);
+            this.radWaitingBar2.Name = "radWaitingBar2";
+            this.radWaitingBar2.Size = new System.Drawing.Size(130, 24);
+            this.radWaitingBar2.TabIndex = 6;
+            this.radWaitingBar2.Text = "radWaitingBar2";
+            this.radWaitingBar2.WaitingIndicators.Add(this.dotsLineWaitingBarIndicatorElement2);
+            this.radWaitingBar2.WaitingIndicatorSize = new System.Drawing.Size(100, 14);
+            this.radWaitingBar2.WaitingSpeed = 80;
+            this.radWaitingBar2.WaitingStyle = Telerik.WinControls.Enumerations.WaitingBarStyles.DotsLine;
+            // 
+            // dotsLineWaitingBarIndicatorElement2
+            // 
+            this.dotsLineWaitingBarIndicatorElement2.ElementColor = System.Drawing.Color.Aqua;
+            this.dotsLineWaitingBarIndicatorElement2.Name = "dotsLineWaitingBarIndicatorElement2";
+            // 
+            // dotsLineWaitingBarIndicatorElement1
+            // 
+            this.dotsLineWaitingBarIndicatorElement1.ElementColor = System.Drawing.Color.IndianRed;
+            this.dotsLineWaitingBarIndicatorElement1.Name = "dotsLineWaitingBarIndicatorElement1";
+            // 
+            // RadForm1
+            // 
+            this.AutoScaleBaseSize = new System.Drawing.Size(7, 15);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(589, 469);
+            this.Controls.Add(this.C3);
+            this.Controls.Add(this.C2);
+            this.Controls.Add(this.C1);
+            this.Controls.Add(this.B3);
+            this.Controls.Add(this.B2);
+            this.Controls.Add(this.B1);
+            this.Controls.Add(this.A3);
+            this.Controls.Add(this.A2);
+            this.Controls.Add(this.A1);
+            this.Controls.Add(this.radPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "RadForm1";
+            // 
+            // 
+            // 
+            this.RootElement.ApplyShapeToControl = true;
+            this.Text = "RadForm1";
+            ((System.ComponentModel.ISupportInitialize)(this.A1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.A2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.A3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.B1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.B2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.B3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.C1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.C2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.C3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnNewGame)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnExit)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radPanel1)).EndInit();
+            this.radPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.radWaitingBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radWaitingBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Telerik.WinControls.UI.RadButton A1;
+        private Telerik.WinControls.UI.RadButton A2;
+        private Telerik.WinControls.UI.RadButton A3;
+        private Telerik.WinControls.UI.RadButton B1;
+        private Telerik.WinControls.UI.RadButton B2;
+        private Telerik.WinControls.UI.RadButton B3;
+        private Telerik.WinControls.UI.RadButton C1;
+        private Telerik.WinControls.UI.RadButton C2;
+        private Telerik.WinControls.UI.RadButton C3;
+        private Telerik.WinControls.UI.RadButton btnNewGame;
+        private Telerik.WinControls.UI.RadButton btnExit;
+        private Telerik.WinControls.UI.RadPanel radPanel1;
+        private Telerik.WinControls.UI.RadWaitingBar radWaitingBar2;
+        private Telerik.WinControls.UI.DotsLineWaitingBarIndicatorElement dotsLineWaitingBarIndicatorElement2;
+        private Telerik.WinControls.UI.RadWaitingBar radWaitingBar1;
+        private Telerik.WinControls.UI.DotsLineWaitingBarIndicatorElement dotsLineWaitingBarIndicatorElement1;
+    }
+}
---
+++ TelerikTask/RadForm1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Telerik.WinControls;
+using Telerik.WinControls.UI;
+
+namespace TelerikWinFormsApp9
+{
+    public partial class RadForm1 : Telerik.WinControls.UI.RadForm
+    {
+        bool player = true; //true = Blue X turn, false = Red O turn
+
+        public RadForm1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_click(object sender, EventArgs e)
+        {
+            RadButton b = (RadButton)sender;
+            if (player == true)
+            {
+                b.ButtonElement.ButtonFillElement.BackColor = Color.Aqua;
+                b.Text = "X";
+                b.ButtonElement.TextElement.ForeColor = Color.Black;
+            }
+            else
+            {
+                b.ButtonElement.ButtonFillElement.BackColor = Color.IndianRed;
+                b.Text = "O";
+                b.ButtonElement.TextElement.ForeColor = Color.Black;
+            }
+            player = !player;
+            b.Enabled = false;
+            checkForWinner();
+        }
+        private void checkForWinner()
+        {
+            //Horizontal
+            bool isWinner = false;
+            if ((A1.Text == A2.Text) && (A2.Text == A3.Text) && (!A1.Enabled))
+            {
+                isWinner = true;
+            }
+
+            else if ((B1.Text == B2.Text) && (B2.Text == B3.Text) && (!B1.Enabled))
+            {
+                isWinner = true;
+            }
+
+            else if ((C1.Text == C2.Text) && (C2.Text == C3.Text) && (!C1.Enabled))
+            {
+                isWinner = true;
+            }
+
+            //Vertical
+            else if ((A1.Text == B1.Text) && (B1.Text == C1.Text) && (!A1.Enabled))
+            {
+                isWinner = true;
+            }
+
+            else if ((A2.Text == B2.Text) && (B2.Text == C2.Text) && (!A2.Enabled))
+            {
+                isWinner = true;
+            }
+
+            else if ((A3.Text == B3.Text) && (B3.Text == C3.Text) && (!A3.Enabled))
+            {
+                isWinner = true;
+            }
+
+            //Diagonally
+            else if ((A1.Text == B2.Text) && (B2.Text == C3.Text) && (!A1.Enabled))
+            {
+                isWinner = true;
+            }
+            else if ((A3.Text == B2.Text) && (B2.Text == C1.Text) && (!A3.Enabled))
+            {
+                isWinner = true;
+            }
+
+
+
+
+
+            if (isWinner)
+            {
+                disableButtons();
+                string winner = "";
+
+                if (player == true)
+                {
+                    winner = "Red";
+                }
+                else
+                {
+                    winner = "Blue";
+                }
+                MessageBox.Show("Winner: " + winner);
+            }
+
+
+        }
+
+        private void disableButtons()
+        {
+            try
+            {
+
+                foreach (Control c in Controls)
+                {
+
+                    RadButton b = (RadButton)c;
+                    b.Enabled = false;
+                }
+            }
+            catch { }
+        }
+
+
+
+        private void btnNewGame_Click_1(object sender, EventArgs e)
+        {
+            player = true;
+
+            try
+            {
+
+                foreach (Control c in Controls)
+                {
+
+                    RadButton b = (RadButton)c;
+                    b.Enabled = true;
+                    b.Text = "";
+                    b.ButtonElement.ButtonFillElement.BackColor = Color.DarkGray;
+                }
+            }
+            catch { }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
